56b4dfd9854a7a2c30ab36f252ad13fa7e3ba7ac
Qu33nArlene <ahaskins93@yahoo.com>
2022-08-12 18:19:21 -0400

    added contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+const [formState, setFormState] = useState({
+  name: "",
+  email: "",
+  message: "",
+});
+const { name, email, message } = formState;
+
+function handleChange(e) {
+  setFormState({ ...formState, [e.target.name]: e.target.value });
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(formState);
+}
 
 function ContactForm() {
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         {/* name input */}
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name"></input>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onChange={handleChange}
+          ></input>
         </div>
         {/* email input */}
         <div>
           <label htmlFor="email">Email Address:</label>
-          <input type="email" name="email"></input>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onChange={handleChange}
+          ></input>
         </div>
         {/* message text area */}
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5"></textarea>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onChange={handleChange}
+          ></textarea>
         </div>
         <button type="submit">Submit</button>
       </form>
